68020e9bc44067daca68dde383cc5802064448af
Mohdtarek54 <mohammedtarek54@gmail.com>
2021-07-10 19:54:02 +0200

    main.c
    final merge

--- Student1/main.c
+++ Student1/main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
 
 typedef struct Student{
 char *name;
@@ -13,14 +13,126 @@ struct Node
 	struct Node* next;
 };
 
+
+void insert_first(struct Student* array,int n,struct Student w){
+    n=n+1;
+    realloc(array,sizeof(struct Student) * (n));
+    int i;
+    for ( i =n-1 ;i>0;i--){
+        array[i]=array[i-1];
+    }
+    array[i]=w;
+}
+
+void insert_last(struct Student* array,int n,struct Student w){
+     n=n+1;
+    realloc(array,sizeof(struct Student) * (n));
+    array[n-1]=w;
+}
+
+void insert_mid(struct Student* array,int  n,struct Student w,int mid){
+    n=n+1;
+    realloc(array,sizeof(struct Student) * (n));
+    int i;
+    for ( i =n-1 ;i > mid;i--)
+        {
+        array[i]=array[i-1];
+    }
+    array[i]=w;
+}
+
 void printStudentData(struct Node *node);
 
 struct Node *head,*tail;
 
 int main()
 {
+    int k;
+    int n;
+    int x;
+    int ch;
+    struct Student w1,w2,w3,w4;
+    printf("For Array Press 1\n");
+     printf("For LinkedList Press 2\n");
+   scanf("%i",&ch);
+if (ch==1)
+{
+
+
+    w1.name ="mohamed";
+    w1.id = 36;
+    w1.day=1;
+    w1.month=3;
+    w1.year=12;
+    w1.lastYearScore=55;
+
+   w2.name ="tarek";
+    w2.id = 362;
+    w2.day=12;
+    w2.month=32;
+    w2.year=122;
+    w2.lastYearScore=552;
+    w3.name ="amr";
+    w3.id = 363;
+    w3.day=13;
+    w3.month=33;
+    w3.year=123;
+    w3.lastYearScore=553;
+    w4.name ="magdy";
+    w4.id = 364;
+    w4.day=14;
+    w4.month=34;
+    w4.year=124;
+    w4.lastYearScore=554;
+    printf("Insert The size of array\n");
+    scanf("%d",&n);
+
+    struct Student* array = (struct Student*)malloc(sizeof(struct Student) * n);
+
+     int i;
+     for( i =0;i<n;i++){
+         array[i]=w1;
+    }
+    printf("To Insert First Press 1\n");
+    printf("To Insert Mid Press 2\n");
+    printf("To Insert last Press 3\n");
+    scanf("%i",&k);
+  if(k==1)
+
+        {insert_first(array,n,w2);
+    n=n+1;}
+    else if (k==2)
+    {printf("insert a number between 1 and %i\n",n);
+    scanf("%d" ,&x);
+    if (x<=0 || x>n)
+         printf("wrong number\n The old List \n");
+insert_mid(array,n,w2,x-1);
+    n++;}
+
+   else if (k==3)
+    {insert_last(array,n,w2);
+    n++;}
+
+    else
+        printf("wrong number\n");
+
+     for(int i =0;i<n;i++){
+              printf("Student%d:\n",i+1);
+         printf("Student%d Name: %s\n",i+1,array[i].name);
+         printf("Student%d ID: %d\n",i+1,array[i].id);
+         printf("Student%d BirthDay: %d / %d / %d\n",i+1,array[i].day,array[i].month,array[i].year);
+         printf("Student%d Last Year score: %d\n",i+1,array[i].lastYearScore);
+         printf("__________________________________\n");
+    }
+
+ return 0;}
+     else if (ch==2)
+ {
+
+
     head = NULL;
     tail = NULL;
+
     generateLinkedList(10, &head, &tail);
 
     struct Student s1, s2, s3, s4, s5;
@@ -67,8 +179,10 @@ int main()
     insertMiddle(s5, 1);
 
     printList(head);
+ } else  {printf("Wrong Number");
 
     return 0;
+}
 }
 
 void pushStudent(struct Student student){
@@ -110,20 +224,6 @@ newNode->next = temp->next;
  temp->next = newNode;
 
 }
-
-/*
-//getting size of the linked list
-int size = 1;
-struct Student *temp = linkedListHeadRef;
-while( temp->nextAddress != NULL){
-        size++;
-        temp = temp->nextAddress;
-}
-    int index = (size / 2) - 1; // we will put the student after that index
- printf("size: %d\n", size);
-
- */
-
 
 struct Node* createStudent(){
 struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
@@ -168,4 +268,5 @@ while(node != NULL){
             printf(" Address of next struct is: %d \n", node->next);
             node = node->next;
 }
+
 }
